690cfc7aeb4939e3be819703cce81f94c7b04936
JunhoYeo <hanaro0704@gmail.com>
2020-07-25 15:52:47 +0900

    test: add test for getPetitions

---
+++ lib/getPetitions.test.ts
@@ -0,0 +1,31 @@
+import getPetitions, {
+  IGetPetitionsReturn,
+  IPetitionsListItem,
+} from './getPetitions';
+
+describe('Get petitions', () => {
+  test('Get petitions', async () => {
+    const {
+      totalPages,
+      currentPage,
+      petitions,
+    }: IGetPetitionsReturn = await getPetitions();
+
+    expect(totalPages).toBeGreaterThan(1000);
+    expect(currentPage).toEqual(1);
+
+    expect(Array.isArray(petitions));
+    expect(petitions).toContainEqual(
+      expect.objectContaining<IPetitionsListItem>({
+        id: expect.any(Number),
+        number: expect.any(Number),
+        title: expect.any(String),
+        category: expect.any(String),
+        provider: expect.any(String),
+        agreementCount: expect.any(Number),
+        createdAt: expect.any(String),
+        finishedAt: expect.any(String),
+      })
+    );
+  });
+});
